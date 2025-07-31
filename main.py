976c562229cf1b658762cdb4d42e6cf9cf23b741
adrianhelllo <hetessyadrian@gmail.com>
2025-07-31 20:01:05 +0200

    aggregation

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-class Animal():
-    is_alive = True
+class Library():
+    def __init__(self, name):
+        self.name = name
+        self.books = []
 
-class Dog(Animal):
-    def speak(self):
-        print('Woof!')
+    def add_book(self, book):
+        self.books.append(book)
 
-class Cat(Animal):
-    def speak(self):
-        print('Meow!')
+    def library_name(self):
+        print(f"Welcome to {self.name} library!")
 
-class Car():
-    is_alive = False
-    def speak(self):
-        print("duda")
+    def list_books(self):
+        print('This library has the following books:')
 
-objects = [Dog(), Cat(), Car()]
+        for book in self.books:
+            print(f'{book.title} by {book.author}')
 
-for object in objects:
-    if object.is_alive:
-        object.speak()
+class Book():
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+book1 = Book('Fasz1', 'Fasz1')
+book2 = Book('Fasz2', 'Fasz2')
+book3 = Book('Fasz3', 'Fasz3')
+
+library = Library('Könyvtár')
+
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+
+library.list_books()
+
+
+        
 
