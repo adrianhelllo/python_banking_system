b1c35249c8ef073ed77c3a726d4eb16967d944c9
adrianhelllo <hetessyadrian@gmail.com>
2025-08-01 16:15:07 +0200

    when using nested classes, it is important to mark ownership of the nested child class to the parent class with . notation:
    Car.Wheel != Wheel

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 class Car:
-    def __init__(self, model, color, horse_p, wheel_s):
-        self.model = model
-        self.color = color
-        self.wheels = [Wheel(19) for _ in range(4)]
-        self.engine = Engine(500)
-
-    def __str__(self):
-        return f'This car is a {self.color} {self.model}, which has {len(self.wheels)}, {self.wheels[0].wheel_size}in wheels, and has an engine with {self.engine.horse_power} HP.'  
-
     class Wheel:
         def __init__(self, wheel_s):
             self.wheel_size = wheel_s
@@ -21,6 +12,15 @@ class Car:
 
         def print_hp(self):
             print(f"Vroom! My horsepower is {self.horse_power}")
+
+    def __init__(self, model, color, horse_p, wheel_s):
+        self.model = model
+        self.color = color
+        self.wheels = [Car.Wheel(19) for _ in range(4)]
+        self.engine = Car.Engine(500)
+
+    def __str__(self):
+        return f'This car is a {self.color} {self.model}, which has {len(self.wheels)}, {self.wheels[0].wheel_size}in wheels, and has an engine with {self.engine.horse_power} HP.'  
 
 my_car = Car("Ford Focus", "Red", "900", 19)
 
