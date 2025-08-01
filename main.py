0e795997bb0dc1b13fe9ca2deaa487eb7836d341
adrianhelllo <hetessyadrian@gmail.com>
2025-08-01 17:55:13 +0200

    property wrappers: property, setter, deleter

--- main.py
+++ main.py
@@ -10,10 +10,46 @@ class Rectangle:
     @property
     def height(self):
         return f"{self._height:.1f}cm"
+    
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
+        else:
+            print("Invalid new width for rectangle.")
+
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Invalid new width for rectangle.")
+
+    @width.deleter
+    def width(self):
+        del self._width
+        print("Private width has been deleted.")
+
+    @height.deleter
+    def height(self):
+        del self._height
+        print("Private height has been deleted.")
 
 
 rectangle = Rectangle(3, 4)
+
+rectangle.width = 6
+rectangle.height = 15
+
+del rectangle.width
+del rectangle.height
+
+rectangle.width = 6
+rectangle.height = 15
+
 print(rectangle.width, rectangle.height)
+
+
         
 
 
