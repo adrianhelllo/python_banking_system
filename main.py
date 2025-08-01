fcf68cbdf9532e2ee9a4d4075c43a03a9c9c232f
adrianhelllo <hetessyadrian@gmail.com>
2025-08-01 16:12:10 +0200

    nested classes

--- main.py
+++ main.py
@@ -6,24 +6,21 @@ class Car:
         self.engine = Engine(500)
 
     def __str__(self):
-        return f'This car is a {self.color} {self.model}, which has {len(self.wheels)}, {self.wheels[0].wheel_size}in wheels, and has an engine with {self.engine.horse_power} HP.'
+        return f'This car is a {self.color} {self.model}, which has {len(self.wheels)}, {self.wheels[0].wheel_size}in wheels, and has an engine with {self.engine.horse_power} HP.'  
 
-        
+    class Wheel:
+        def __init__(self, wheel_s):
+            self.wheel_size = wheel_s
 
-class Wheel:
-    def __init__(self, wheel_s):
-        self.wheel_size = wheel_s
+        def print_size(self):
+            print(f"My wheel size is {self.wheel_size}in.")
 
-    def print_size(self):
-        print(f"My wheel size is {self.wheel_size}in.")
+    class Engine:
+        def __init__(self, horse_p):
+            self.horse_power = horse_p
 
-
-class Engine:
-    def __init__(self, horse_p):
-        self.horse_power = horse_p
-
-    def print_hp(self):
-        print(f"Vroom! My horsepower is {self.horse_power}")
+        def print_hp(self):
+            print(f"Vroom! My horsepower is {self.horse_power}")
 
 my_car = Car("Ford Focus", "Red", "900", 19)
 
