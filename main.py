5113e35530a394b57c58adee12a06436bba768e8
adrianhelllo <hetessyadrian@gmail.com>
2025-08-01 17:20:10 +0200

    class methods: takes cls (class) instead of self.

--- main.py
+++ main.py
@@ -1,50 +1,35 @@
-class Car:
-    class Wheel:
-        def __init__(self, wheel_s):
-            self.wheel_size = wheel_s
+class Student:
+    count = 0
 
-        def print_size(self):
-            print(f"My wheel size is {self.wheel_size}in.")
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Student.count += 1
 
-    class Engine:
-        def __init__(self, horse_p):
-            self.horse_power = horse_p
-
-        def print_hp(self):
-            print(f"Vroom! My horsepower is {self.horse_power}")
-
-    def __init__(self, model, color, horse_p, wheel_s):
-        self.model = model
-        self.color = color
-        self.wheels = [Car.Wheel(wheel_s) for _ in range(4)]
-        self.engine = Car.Engine(horse_p)
-
-    def __str__(self):
-        return f'This car is a {self.color} {self.model}, which has {len(self.wheels)}, {self.wheels[0].wheel_size}in wheels, and has an engine with {self.engine.horse_power} HP.'  
+    #INSTANCE METHOD
+    def student_info(self):
+        print(f"\nStudent info:\n| Name: {self.name}\n| GPA: {self.gpa}")
 
     @staticmethod
-    def drive_car():
-        print("Vroom! You drive the car.")
+    def is_failing_grade(gpa):
+        if gpa < 1.5:
+            print('This GPA would be considered failing.')
+        else:
+            print("This GPA would be considered passing.")
 
-    @staticmethod
-    def stop_car():
-        print("You stopped the car.")
+    @classmethod
+    def get_count(cls):
+        print(f"Total number of students: {cls.count}")
 
-    @staticmethod
-    def is_valid_wheel_number(num):
-        if num < 3 or 4 < num:
-            return print("Invalid number of wheels")
+student1 = Student("Spongebob", 2.4)
+student2 = Student("Csaba", 3.4)
+student3 = Student("Nathan", 0.1)
+student4 = Student("Levendulás", 10)
+
+Student.get_count()
+
+for student in (student1, student2, student3, student4):
+    student.student_info()
         
-        return print("Valid number of wheels")
 
-
-Car.drive_car()
-Car.stop_car()
-
-my_car = Car("Ford Focus", "Red", 900, 19)
-
-print(my_car)
-
-
-        
 
