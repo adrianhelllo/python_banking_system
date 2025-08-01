0d7407e72b6cdaecb50fd69934a440c4cdd86fc4
adrianhelllo <hetessyadrian@gmail.com>
2025-08-01 16:54:00 +0200

    static method:
    Use for utility functions that don't require class / instance data. Doesn't require self / cls.

--- main.py
+++ main.py
@@ -16,13 +16,32 @@ class Car:
     def __init__(self, model, color, horse_p, wheel_s):
         self.model = model
         self.color = color
-        self.wheels = [Car.Wheel(19) for _ in range(4)]
-        self.engine = Car.Engine(500)
+        self.wheels = [Car.Wheel(wheel_s) for _ in range(4)]
+        self.engine = Car.Engine(horse_p)
 
     def __str__(self):
         return f'This car is a {self.color} {self.model}, which has {len(self.wheels)}, {self.wheels[0].wheel_size}in wheels, and has an engine with {self.engine.horse_power} HP.'  
 
-my_car = Car("Ford Focus", "Red", "900", 19)
+    @staticmethod
+    def drive_car():
+        print("Vroom! You drive the car.")
+
+    @staticmethod
+    def stop_car():
+        print("You stopped the car.")
+
+    @staticmethod
+    def is_valid_wheel_number(num):
+        if num < 3 or 4 < num:
+            return print("Invalid number of wheels")
+        
+        return print("Valid number of wheels")
+
+
+Car.drive_car()
+Car.stop_car()
+
+my_car = Car("Ford Focus", "Red", 900, 19)
 
 print(my_car)
 
